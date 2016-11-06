5ceace3fec747ad2832ff4b2bc6692407a84b930
sibicramesh <sibiterrors@gmail.com>
2016-11-05 21:38:12 -0500

    changed code

--- source/www/js/app.js
+++ source/www/js/app.js
@@ -3,8 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
+var test=angular.module('start', ['ionic'] )
 var exampleApp=angular.module('starter', ['ionic'])
-var test=angular.module('start', ['ionic'])
+
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
@@ -44,27 +45,78 @@ function startScan() {
 
 }
        exampleApp.controller('ExampleController', function($scope, $http) {
- 
-    $scope.getData = function() {
-        $http.get("http://api.walmartlabs.com/v1/items?ids=16821022,44436664,33282383,44981963,10295586,33282297&apiKey=9gnk426wzsb972r7xmumfaxr&format=json")
-            .success(function(data) {
-            
-              
-            $scope.price0= data.items[0].salePrice;
-            
+      $scope.inputs = [{
+        value: null
+    }];
+           $scope.prices = [{
+               "price":[]
+           }];
+         $scope.field=function () {
+           $scope.inputs.push({
+             value: null
+           })
+         }
+    $scope.addInput = function () {
+
+      $http.get("http://api.walmartlabs.com/v1/items?ids=16821022,44436664,33282383,44981963,10295586,33282297&apiKey=9gnk426wzsb972r7xmumfaxr&format=json")
+        .success(function(data) {
+          for(var i=0;i<$scope.inputs.length;i++) {
+          if($scope.inputs[i].value=='pepsi'){
+
+            console.log(data.items[0].salePrice);
+              $scope.price0= data.items[0].salePrice;
+
+              $scope.prices.push($scope.price0);
+            }
+          else if($scope.inputs[i].value=='b'){
+            console.log(data.items[1].salePrice);
+              console.log($scope.prices);
             $scope.price1= data.items[1].salePrice;
+
+              $scope.prices.push($scope.price1);}
+          else if($scope.inputs[i].value=='c'){
+            console.log(data.items[2].salePrice);
             $scope.price2= data.items[2].salePrice;
+              $scope.prices.push($scope.price2);
+           }
+          else if($scope.inputs[i].value=='d'){
+            console.log(data.items[3].salePrice);
             $scope.price3= data.items[3].salePrice;
-            $scope.price4= data.items[4].salePrice;
+            ;
+              $scope.prices.push($scope.price3)}
+          else if($scope.inputs[i].value=='e'){
+            console.log(data.items[4].salePrice);
+            $scope.price4= data.items[4].salePrice
+              $scope.prices.push($scope.price4);;
+              }
+          else if($scope.inputs[i].value=='f'){
+            console.log(data.items[5].salePrice);
             $scope.price5= data.items[5].salePrice;
-            
-            
-            
-            })
-            .error(function(data) {
-                //alert("ERROR");
-            });
-       
+              $scope.prices.push($scope.price5);
+          }
+          }
+
+          //$scope.price0= data.items[0].salePrice;
+          //$scope.price1= data.items[1].salePrice;
+          //$scope.price2= data.items[2].salePrice;
+          //$scope.price3= data.items[3].salePrice;
+          //$scope.price4= data.items[4].salePrice;
+          //$scope.price5= data.items[5].salePrice;
+
+
+
+        })
+        .error(function(data) {
+          //alert("ERROR");
+        });
+
+    }
+
+    $scope.removeInput = function (index) {
+        $scope.inputs.splice(index, 1);
+    }
+    $scope.getData = function() {
+
     }
    
  
